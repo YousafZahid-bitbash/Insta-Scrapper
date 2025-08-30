a491d39bccd18baa808136cd7c716334cf4ee7f0
Yousaf Zahid <yousaf.zahid@bitbash.dev>
2025-08-30 16:26:32 +0500

    fix

--- src/app/dashboard/purchase/page.tsx
+++ src/app/dashboard/purchase/page.tsx
@@ -1,12 +1,15 @@
+"use client";
+import Script from "next/script";
+import PurchaseClient from "./PurchaseClient";
 
-// import PurchaseClient from "./PurchaseClient";
-// type SearchParams = { [key: string]: string | string[] | undefined };
+type SearchParams = { [key: string]: string | string[] | undefined };
 
-// export default function PurchasePage({ searchParams }: { searchParams?: SearchParams }) {
-//   const deal = {
-//     name: typeof searchParams?.name === "string" ? searchParams.name : "",
-//     price: typeof searchParams?.price === "string" ? searchParams.price : "",
-//     coins: typeof searchParams?.coins === "string" ? searchParams.coins : "",
-//   };
-//   return <PurchaseClient deal={deal} />;
-// }
+export default function PurchasePage({ searchParams }: { searchParams?: SearchParams }) {
+  const deal = {
+    name: typeof searchParams?.name === "string" ? searchParams.name : "",
+    price: typeof searchParams?.price === "string" ? searchParams.price : "",
+    coins: typeof searchParams?.coins === "string" ? searchParams.coins : "",
+  };
+  return <PurchaseClient deal={deal} />;
+}
+      <Script src={`https://js.crypto.com/sdk?publishable-key=${process.env.NEXT_PUBLIC_CRYPTO_PUBLISHABLE_KEY}`} strategy="afterInteractive" />
